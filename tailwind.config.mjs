1ca7334e723ffe00a79a7150e9d96132fe1a0be0
Miguel Téllez <miguel.tellez1997@gmail.com>
2024-06-27 12:45:01 -0600

    Configure Tailwind theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: '#E8F4F2',
+          DEFAULT: '#2A8E84',
+        },
+        accent: '#003366',
+        'gray-20': '#333333',
+        code: '#EEEEEE',
+      },
+    },
+  },
+  plugins: [],
+};
